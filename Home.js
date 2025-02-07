f134278243c59ce0df6a9d70166e40c3f0362ded
Wendy192005 <nanini140@gmail.com>
2025-02-07 18:08:48 -0500

    Update Home.js

---
+++ Home.js
@@ -0,0 +1,14 @@
+import styles from '../styles/Home.module.css';
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Welcome to My React App!</h1>
+      <p className={styles.description}>
+        This is a simple web application demonstrating React concepts.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
